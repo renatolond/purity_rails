7c007c44de6d8722b114322a87acc7c9eea4c374
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2016-03-01 23:40:29 +0100

    Create user model.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'admin@lond.com.br'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -145,7 +145,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 8..72
+  config.password_length = 8..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
@@ -239,6 +239,14 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  #config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
+  #                scope: 'email,user_friends',
+  #                client_options: {
+  #                  site: 'https://graph.facebook.com/v2.2',
+  #                  authorize_url: 'https://www.facebook.com/v2.2/dialog/oauth'
+  #                }
+  #config.omniauth :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET']
+  #config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
---
+++ db/migrate/20160301223954_add_name_nickname_to_user.rb
@@ -0,0 +1,6 @@
+class AddNameNicknameToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :nickname, :string
+  end
+end

7fa6bb28e86f01d17a6481613119566b6d2ff3d7
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2016-03-02 23:09:56 +0100

    Allow login with nickname.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,36 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_and_belongs_to_many :questions
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(nickname) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:nickname) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  validates :nickname,
+    :presence => true,
+    :uniqueness => {
+      :case_sensitive => false
+    }
+
+  validate :validate_nickname
+
+  def validate_nickname
+    if User.where(email: nickname).exists?
+      errors.add(:nickname, :invalid)
+    end
+  end
+
+  attr_accessor :login
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.nickname || self.email
+  end
 end

0aeee9bebde8106adc7b23aa9efac1163e165df2
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2016-03-02 20:29:26 +0100

    Add test controller with index and show actions.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'tests#index'
+
+  resources :tests
+  resources :groups
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

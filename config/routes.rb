aa35fd5cbc317a5e36d69e353028199c8ad32144
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2016-03-02 21:48:17 +0100

    Add group controler only with show action.

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'tests#index'
 
-  resources :tests
-  resources :groups
+  resources :tests, only: [:index, :show]
+  resources :groups, only: [:show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
